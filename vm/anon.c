64680312db0305f04c819a8059ea239941db0ea0
Recordum <mingyu.oh15b@gmail.com>
2023-05-21 05:23:51 +0000

    [feat]anon_swap_in impl

--- vm/anon.c
+++ vm/anon.c
@@ -2,13 +2,19 @@
 
 #include "vm/vm.h"
 #include "devices/disk.h"
-
+#include "threads/vaddr.h"
 /* DO NOT MODIFY BELOW LINE */
 static struct disk *swap_disk;
 static bool anon_swap_in (struct page *page, void *kva);
 static bool anon_swap_out (struct page *page);
 static void anon_destroy (struct page *page);
+struct slot* find_swap_slot(struct page *swap_page);
 
+enum swap{
+ 	SWAP_OUT = false,
+	SWAP_IN  = true,
+	SECOTR_PER_SLOT = PGSIZE/DISK_SECTOR_SIZE,
+};
 /* DO NOT MODIFY this struct */
 static const struct page_operations anon_ops = {
 	.swap_in = anon_swap_in,
@@ -24,7 +30,8 @@ vm_anon_init (void) {
 	swap_disk = disk_get(1,1);
 	list_init(&swap_table);
 	disk_sector_t sector_number = disk_size(swap_disk); //size for swaptable
-	int slot_number = sector_number / 8;
+	int slot_number = (sector_number + 1) / SECOTR_PER_SLOT;
+	
 	for (int i = 0 ; i < slot_number ; i++){
 		struct slot *uninit_slot = malloc(sizeof(struct slot));
 		uninit_slot->slot_number = i;
@@ -47,9 +54,14 @@ anon_initializer (struct page *page, enum vm_type type, void *kva) {
 static bool
 anon_swap_in (struct page *page, void *kva) {
 	struct anon_page *anon_page = &page->anon;
-	/*
-	insert new frame
-	*/
+	struct slot *swap_slot= find_swap_slot(page);
+	disk_sector_t sector_number = swap_slot->slot_number * SECOTR_PER_SLOT;
+
+	int offset = 0;
+	for (int i = sector_number - SECOTR_PER_SLOT ; i< sector_number ; i++){
+		disk_read(swap_disk, i, (char*)kva + (DISK_SECTOR_SIZE * offset));
+		offset ++;
+	}
 
 }
 
@@ -57,26 +69,29 @@ anon_swap_in (struct page *page, void *kva) {
 static bool
 anon_swap_out (struct page *page) {
 	struct anon_page *anon_page = &page->anon;
-
-	struct slot *swap_slot= find_swap_slot();
+	struct slot *swap_slot= find_swap_slot(NULL);
 	swap_slot->page = page;
-	disk_sector_t sector_number = swap_slot->slot_number * 8;
-	for (int i = sector_number - 8 ; i < sector_number ; i++){
-		disk_write(swap_disk, i, page->frame->kva);
+	disk_sector_t sector_number = swap_slot->slot_number * SECOTR_PER_SLOT;
+
+	int offset = 0;
+	for (int i = sector_number - SECOTR_PER_SLOT ; i < sector_number ; i++){
+		disk_write(swap_disk, i, (char*)(page->frame->kva) + (offset * DISK_SECTOR_SIZE));
+		offset ++;
 	}
 	palloc_free_page(page->frame->kva);
 	pml4_clear_page(thread_current()->pml4, page->va);
 }
 
 struct slot*
-find_swap_slot(){
+find_swap_slot(struct page *swap_page){
+
 	struct list_elem *slot_elem = list_begin(&swap_table);
 	while(true){
 		struct slot *swap_slot = list_entry(slot_elem, struct slot, swap_elem);
 		if (swap_slot == list_end(&swap_table)){
 			PANIC("OVER CAPACITY LIMIT");
 		}
-		if (swap_slot->page == NULL){
+		if (swap_slot->page == swap_page){
 			return swap_slot;
 		}
 	}
